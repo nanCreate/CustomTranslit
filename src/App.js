0ae3152e0df27242ba04dc4e4eacb00c73b05435
tykva2 <nan.create@proton.me>
2022-12-10 11:47:18 +0900

    Initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import './App.css'
+import * as redux from 'react-redux'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Custom Translit</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Custom Translit</h1>
+		</div>
+	)
 }
 
-export default App;
+export default App
