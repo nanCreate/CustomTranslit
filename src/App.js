b657213f5bee0fd7b4a6fa8b113c760be22b022b
tykva2 <nan.create@proton.me>
2022-12-10 15:42:38 +0900

    Automatic copy text onChange

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css'
-import * as redux from 'react-redux'
+import {useState} from 'react'
 
 function App() {
+	const toCopy = (text) => {
+		navigator.clipboard.writeText(text)
+	}
+
+	const [text, setText] = useState('')
+
 	return (
 		<div className="App">
 			<h1>Custom Translit</h1>
+
+			<textarea
+				onChange={(e) => {
+					setText(e.target.value)
+					toCopy(e.target.value)
+				}}
+				value={text}
+				cols="80"
+				rows="20"
+				autoFocus={true}
+				placeholder={'Начинайте вводить текст'}
+			/>
 		</div>
 	)
 }
