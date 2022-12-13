a1cc401854757bb56167df4504fc513838ad8ac5
tykva2 <nan.create@proton.me>
2022-12-14 08:44:16 +0900

    Routes: - Main page - Settings page - About page

--- src/App.js
+++ src/App.js
@@ -3,35 +3,23 @@ import {useState} from 'react'
 import TextAreaAutosize from 'react-textarea-autosize'
 import {setTest, test} from './redux/translitModels-reducer'
 import {useSelector, useDispatch} from 'react-redux'
-import {Button, InputText, NavBar, NavBarLink, NavPageContainer} from 'react-windows-ui'
+import {
+	Button,
+	InputText,
+	NavBar,
+	NavBarLink,
+	NavBarSubMenu,
+	NavPageContainer,
+	RadioButton,
+} from 'react-windows-ui'
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
-	const [text, setText] = useState('')
-	const [textTranslit, setTextTranslit] = useState('')
 	const [previewStatus, setPreviewStatus] = useState(true)
-
-	const toCopy = (text) => {
-		setTextTranslit(text)
-		navigator.clipboard.writeText(text)
-	}
 
 	const translitModel = useSelector((state) => state.translitModels.gostB)
 	const appTitle = useSelector((state) => state.translitModels.title)
 	const dispatch = useDispatch()
-	const translit = (word, model) => {
-		let answer = ''
-		const converter = model.alphabet
-
-		for (let i = 0; i < word.length; ++i) {
-			if (converter[word[i]] === undefined) {
-				answer += word[i]
-			} else {
-				answer += converter[word[i]]
-			}
-		}
-
-		return answer
-	}
 
 	const togglePreview = () => {
 		if (previewStatus) {
@@ -43,33 +31,35 @@ function App() {
 		}
 	}
 
-	return (
-		<div>
-			{/*<aside>*/}
-			<NavBar
-				title="Custom Translit"
-				shadowOnScroll={true}
-				titleBarMobile={
-					<div>
-						<span className="app-navbar-name">React-windows-ui</span>
-					</div>
-				}
-			>
-				<NavBarLink to="/" exact={true} text="Page1" icon={<i className="icons10-home"></i>} />
-				<NavBarLink
-					to="/settings"
-					exact={true}
-					text="Page1"
-					icon={<i className="icons10-home"></i>}
-				/>
-			</NavBar>
-			{/*<button onClick={togglePreview}>👁️</button>*/}
-			{/*<button onClick={() => dispatch(test('Hello world'))}>🔬</button>*/}
-			{/*</aside>*/}
+	// TO own Main page Component
+	const MainPage = () => {
+		const [text, setText] = useState('')
+		const [textTranslit, setTextTranslit] = useState('')
 
+		const toCopy = (text) => {
+			setTextTranslit(text)
+			navigator.clipboard.writeText(text)
+		}
+		const translit = (word, model) => {
+			let answer = ''
+			const converter = model.alphabet
+
+			for (let i = 0; i < word.length; ++i) {
+				if (converter[word[i]] === undefined) {
+					answer += word[i]
+				} else {
+					answer += converter[word[i]]
+				}
+			}
+
+			return answer
+		}
+
+		return (
 			<NavPageContainer hasPadding={true} animateTransition={true}>
 				<div className="App">
-					<header>Custom Translit: {appTitle}</header>
+					<header></header>
+
 					<div className="TranslateOriginal">
 						<TextAreaAutosize
 							onChange={(e) => {
@@ -87,6 +77,61 @@ function App() {
 					</div>
 				</div>
 			</NavPageContainer>
+		)
+	}
+
+	// Clear page temp
+	const TempClearPage = (props) => {
+		return (
+			<NavPageContainer hasPadding={true} animateTransition={true}>
+				<div className="App">
+					<header>{props.text}</header>
+				</div>
+			</NavPageContainer>
+		)
+	}
+
+	return (
+		<div>
+			{/*<aside>*/}
+			<NavBar
+				title="Custom Translit"
+				shadowOnScroll={true}
+				titleBarMobile={
+					<div>
+						<span className="app-navbar-name">Custom Translit</span>
+					</div>
+				}
+			>
+				<NavBarLink
+					to="/"
+					exact={true}
+					text="Транслит"
+					icon={<i className="icons10-keyboard"></i>}
+				/>
+
+				<NavBarLink
+					to="/settings"
+					exact={true}
+					text="Настройки"
+					icon={<i className="icons10-settings"></i>}
+				/>
+				<NavBarLink
+					to="/about"
+					exact={true}
+					text="О программе"
+					icon={<i className="icons10-info"></i>}
+				/>
+			</NavBar>
+			{/*<button onClick={togglePreview}>👁️</button>*/}
+			{/*<button onClick={() => dispatch(test('Hello world'))}>🔬</button>*/}
+			{/*</aside>*/}
+
+			<Routes>
+				<Route path={'/'} element={<MainPage />} />
+				<Route path={'/settings'} element={<TempClearPage text={'page settings'} />} />
+				<Route path={'/about'} element={<TempClearPage text={'page about'} />} />
+			</Routes>
 		</div>
 	)
 }
