f9534288b7bbb402df144dc252bfd844b2c1da72
tykva2 <nan.create@proton.me>
2023-02-11 15:37:49 +0900

    Refactor Transliter

--- src/lib/transliter.js
+++ src/lib/transliter.js
@@ -1,14 +1,7 @@
 const transliter = (text, model) => {
-	let result = ''
-
-	for (let i = 0; i < text.length; ++i) {
-		if (model[text[i]] === undefined) {
-			result += text[i]
-		} else {
-			result += model[text[i]]
-		}
-	}
-
-	return result
+	return [...text].reduce((result, char) => {
+		return result + (model[char] === '' ? '' : model[char] || char)
+	}, '')
 }
+
 export default transliter
